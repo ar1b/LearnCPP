fd0206eb95c26a03d6d5e0a1af78f9f9fa228b0c
ar1b <arib.kamal99@gmail.com>
2025-03-05 00:08:04 -0500

    adding section 6, testing

---
+++ Section6/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by Arib Kamal on 2025-03-05.
+//
+
+#include "main.hpp"
---
+++ Section6/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  LearnCPP
+//
+//  Created by Arib Kamal on 2025-03-05.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
