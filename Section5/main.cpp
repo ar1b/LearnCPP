0c8f0b160d7a2bdb77659ddb62d84e38a1dc333e
ar1b <arib.kamal99@gmail.com>
2025-03-05 00:05:20 -0500

    Initialized repo, first commit, added Section 5

---
+++ Section5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by Arib Kamal on 2025-03-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
